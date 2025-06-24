b108bdc5a40d3f13189d375ef74de87caaefeff7
anwerESS <43082478+anwerESS@users.noreply.github.com>
2025-06-24 23:34:22 +0200

    add main and note.txt

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> // Includes the iostream library for input/output operations
+
+
+
+
+int main() { // Defines the main function, which returns an integer
+    // This is a comment. It explains the code and is ignored by the compiler.
+    std::cout << "Hello, World!" << std::endl; // Prints "Hello, World!" to the console
+
+    return 0; // Returns 0 to indicate successful program execution
+}
